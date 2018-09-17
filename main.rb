6af9a74a2dc23e9ba27d3cc4c8826cb4120e7f2f
Kenny Wu <kenny.wu@lendesk.com>
2018-09-17 14:35:00 -0700

    Add Basic Routing

--- main.rb
+++ main.rb
@@ -1,7 +1,46 @@
 class App
+  def initialize(&block)
+    @routes = RoutesTable.new(&block)
+  end
+
   def call(env)
-    [200, {}, ['Hello World']]
+    request = Rack::Request.new(env)
+    @routes.each do |route|
+      content = route.match(request)
+      return [200, {}, [content]] if content
+    end
+    [404, {}, ['Page does not exist']]
+  end
+
+  class RoutesTable
+    def initialize(&block)
+      @routes = []
+      instance_eval(&block)
+    end
+
+    def each(&block)
+      @routes.each(&block)
+    end
+
+    def get(route_spec, &block)
+      @routes << Route.new(route_spec, block)
+    end
+  end
+
+  class Route < Struct.new(:route_spec, :block)
+    def match(request)
+      block.call() if request.path == route_spec
+      nil
+    end
   end
 end
 
-APP = App.new()
+APP = App.new do
+  get '/' do
+    'this is the root!'
+  end
+
+  get '/users/:username' do
+    'this is the users page!'
+  end
+end
